72d02d70c1008ecc1bd21c81e79924cf2efe93ba
annisadiann <annisadian0412@gmail.com>
2025-11-07 15:17:02 +0700

    lengkapi input status perkawinan dan alamat pengguna

--- app/src/main/java/com/example/praktikum5tugas2/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5tugas2/view/Formulir.kt
@@ -170,3 +170,28 @@ fun FormDataDiri1(
                 }
             }
 
+            Text(
+                text = "ALAMAT",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 20.dp, bottom = 8.dp)
+            )
+            OutlinedTextField(
+                value = textAlamat,
+                singleLine = true,
+                placeholder = { Text(text = "Alamat", color = Color.Gray) },
+                shape = RoundedCornerShape(8.dp),
+                onValueChange = {
+                    textAlamat = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedBorderColor = Color.LightGray,
+                    unfocusedBorderColor = Color.LightGray
+                )
+            )
+        }
+
