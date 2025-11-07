0a2e94ff079238926b839cfce3832034586aa6b0
annisadiann <annisadian0412@gmail.com>
2025-11-07 15:37:52 +0700

    setup struktur dasar MainActivity dan tema Compose

---
+++ app/src/main/java/com/example/praktikum5tugas2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum5tugas2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum5tugas2.navigation.PengaturNavigasi
+import com.example.praktikum5tugas2.ui.theme.Praktikum5Tugas2Theme
+
+
